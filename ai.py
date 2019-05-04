07e0a930447d0eaf925a37835b40f57a82c60a50
George Codat Tudoran <office@solcreation.ro>
2019-05-04 14:36:32 +0300

    init function for the network class

--- ai.py
+++ ai.py
@@ -6,8 +6,22 @@ import numpy as np
 import random
 import os
 import torch
-import torch.nn
+import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.autograd as autograd
 from torch.autograd import Variable
+
+
+# Creating the architecture of the Nurall Network
+
+class Network(nn.Module):
+    
+    #input_size -> Vectors:  3 signals and orientation / -orientation
+    #nb_action ->  Actions: left, right and straight
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
