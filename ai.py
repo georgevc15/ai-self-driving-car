c07ee75fb1666ec4926c5e9d2d94f4afbc12dabb
George Codat Tudoran <office@solcreation.ro>
2019-05-04 11:53:35 +0300

    Import the libraries needed for building the AI brain for the car

--- ai.py
+++ ai.py
@@ -1,3 +1,13 @@
 # AI for Self Driving Car
 
 # Importing the libraries
+
+import numpy as np
+import random
+import os
+import torch
+import torch.nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
