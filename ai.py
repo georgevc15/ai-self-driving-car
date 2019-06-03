2b0a70019ea1f6147b2311776813849ff1ada726
George Codat Tudoran <office@solcreation.ro>
2019-06-03 15:54:24 +0300

    save the model

--- ai.py
+++ ai.py
@@ -100,6 +100,11 @@ class ReplayMemory(object):
         
         def score(self):
             return sum(self.reward_window)/(len(self.reward_window)+1)
+        
+        def save(self):
+            torch.save({ 'state_dict' : self.model.state_dict(),
+                        'optimizer' : self.optimizer.state_dict
+                        }, 'last_brain.pth')
 
             
                 
